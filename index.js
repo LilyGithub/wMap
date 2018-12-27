2cb424dd0ab8e2a16cadab829f329edc47e38e1f
LilyGithub <912106840432@njust.edu.cn>
2018-12-27 18:43:07 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import { DatePicker } from 'antd';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './app.js';
+
+ReactDOM.render(<App width="1000" height="1000" virtualHeight="100" virtualWidth="100" oy="1000" />, document.getElementById("app"));

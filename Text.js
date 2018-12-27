5400c18c3f62f86b0c0b74d170a91cb6be337f8d
LilyGithub <912106840432@njust.edu.cn>
2018-12-27 18:41:10 +0800

    Create Text.js

---
+++ Text.js
@@ -0,0 +1,15 @@
+import Element from "./Element.js";
+
+export default class Text extends Element{
+    constructor(props){
+        super(props);
+        this.type = "text";
+    }
+    //this.content = options.content;
+    //this.font = options.font;
+    //this.color = options.color;
+    //this.fontSize = options.fontSize;
+    //this.marginLeft = options.marginLeft;
+    //this.marginTop = options.marginTop;
+    //this.attr = options.attr;
+};

da654d3e7f99c85a18f6220d742c3046b486e778
LilyGithub <912106840432@njust.edu.cn>
2018-12-27 18:39:09 +0800

    Update Element.js

--- Element.js
+++ Element.js
@@ -1,17 +1,34 @@
-class Element{
+import {Animal} from "./Animal.js";
+export default class Element{
     constructor(props){
-        super(props);
+        let defultOption = {
+            type:"element",
+            marginTop:0,
+            marginLeft:0,
+            width:1,
+            height:1,
+            sort:"",
+            attr:{}
+        };
+        Object.assign(this,defultOption, props);
+
+        this.moveTo = this.moveTo.bind(this);
+        this.click = this.click.bind(this);
+        this.hover = this.hover.bind(this);
+        this.leave = this.leave.bind(this);
+        this.dblClick = this.dblClick.bind(this);
+        this.release = this.release.bind(this);
     }
 
-    moveTo = function(options){
+    moveTo(options){
         if(this.runing){
             this.animal.waiter = {};
             this.animal.waiter.reLoad = function(animal){
-                animal.times = options.time/fps;
+                animal.times = options.time/____wMap_fps;
                 var dx = options.x - animal.performer.x;
                 var dy = options.y - animal.performer.y;
-                animal.performer.px = (dx/options.time)*fps;
-                animal.performer.py = (dy/options.time)*fps;
+                animal.performer.px = (dx/options.time)*____wMap_fps;
+                animal.performer.py = (dy/options.time)*____wMap_fps;
                 animal.waiter = false;
                 animal.finish = options.finish;
                 animal.start();
@@ -20,23 +37,23 @@ class Element{
             this.runing = true;
             var dx = options.x - this.x;
             var dy = options.y - this.y;
-            this.px = (dx/options.time)*fps;
-            this.py = (dy/options.time)*fps;//动画fps 1000/60
-            this.animal = new _moveRun(this);
+            this.px = (dx/options.time)*____wMap_fps;
+            this.py = (dy/options.time)*____wMap_fps;//动画fps 1000/60
+            this.animal = new Animal(this);
             this.animal.performer = this;
-            this.animal.times = options.time/fps;
+            this.animal.times = options.time/____wMap_fps;
             this.animal.finish = options.finish;
             this.animal.start();
         }
     }
-    click = function(func){
+    click(func){
         if(func){
             this._click = func;return true;
         }else{
             if(this._click)return this._click.call(this);
         }
     };
-    hover = function(func){
+    hover(func){
         if(func){
             this._hover = func;return true;
         }else{
@@ -46,7 +63,7 @@ class Element{
             }
         }
     };
-    leave = function(func){
+    leave(func){
         if(func){
             this._leave = func;return true;
         }else{
@@ -56,14 +73,14 @@ class Element{
             }
         }
     };
-    dblClick = function(func){
+    dblClick(func){
         if(func){
             this._dblClick = func;return true;
         }else{
             if(this._dblClick)return this._dblClick.call(this);
         }
     };
-    release = function(){
+    release(){
         var mk = this;
         if(mk&&mk.animal){
             for(var i in mk.animal){

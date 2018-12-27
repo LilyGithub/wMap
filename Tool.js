78270daf5e19b95e0e401c80e1ced46f41da8fe7
LilyGithub <912106840432@njust.edu.cn>
2018-12-27 18:41:35 +0800

    Update Tool.js

--- Tool.js
+++ Tool.js
@@ -1,15 +1,17 @@
 class Tool{
     constructor(props){
-        super(props);
-
-        this._realeseObject = this._realeseObject.bind(this);
+        
     }
 
-   
-    _realeseObject = function(obj){
+    _realeseObject(obj){
     	for(var i in obj){
     		obj[i] = null;
     	}
     	obj = null;
     }
+
+    
 }
+
+const tool = new Tool();
+export {Tool as tool}
